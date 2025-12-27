3a06a4a7c96aa96d2600e58e65f5a87afaad2f4c
CaliCodes <calista.jajalla@ftwfoundation.org>
2025-12-27 15:12:02 +0800

    Update db.py

--- etl/db.py
+++ etl/db.py
@@ -1,27 +1,29 @@
 # The DB port is for both local/cloud (although you can just use local)
 
-import os
 import psycopg2
 import streamlit as st
 
-# Check if running in Streamlit Cloud or local
-if "SECRETS" in st.secrets:  # Streamlit Cloud
-    DB_CONFIG = {
-        "host": st.secrets["database"]["host"],
-        "port": st.secrets["database"]["port"],
-        "dbname": st.secrets["database"]["name"],
-        "user": st.secrets["database"]["user"],
-        "password": st.secrets["database"]["pass"]
-    }
-else:  # Local fallback
-    DB_CONFIG = {
-        "host": "localhost",
-        "port": 5432,
-        "dbname": "nochebuena",
-        "user": "noche_user",
-        "password": "noche_pass"
-    }
+# Default local config
+LOCAL_DB = {
+    "host": "localhost",
+    "port": 5432,
+    "dbname": "nochebuena",
+    "user": "noche_user",
+    "password": "noche_pass"
+}
 
 def get_connection():
     """Return a new database connection"""
-    return psycopg2.connect(**DB_CONFIG)
+    if "database" in st.secrets:
+        cfg = st.secrets["database"]
+        db_config = {
+            "host": cfg["host"],
+            "port": cfg["port"],
+            "dbname": cfg["name"],
+            "user": cfg["user"],
+            "password": cfg["pass"]
+        }
+    else:
+        db_config = LOCAL_DB
+
+    return psycopg2.connect(**db_config)
